709baf926bacf5fe2c7b5f7a1c71af2a78b3daa6
Mikolaj Bogudal <mbogudal@gmail.com>
2025-12-16 14:57:43 +0100

    init

---
+++ src/main/java/pl/bogudal/mikolaj/GatewayWithKeycloak/GatewayWithKeycloakApplication.java
@@ -0,0 +1,13 @@
+package pl.bogudal.mikolaj.GatewayWithKeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayWithKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayWithKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/bogudal/mikolaj/GatewayWithKeycloak/GatewayWithKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bogudal.mikolaj.GatewayWithKeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayWithKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

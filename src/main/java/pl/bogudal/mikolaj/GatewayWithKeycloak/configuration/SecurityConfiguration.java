fff8fad69505773587062d14c48999d47f109a08
Mikolaj Bogudal <mbogudal@gmail.com>
2025-12-18 15:23:42 +0100

    working on fixing bugs

--- src/main/java/pl/bogudal/mikolaj/GatewayWithKeycloak/configuration/SecurityConfiguration.java
+++ src/main/java/pl/bogudal/mikolaj/GatewayWithKeycloak/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package pl.bogudal.mikolaj.GatewayWithKeycloak.configuration;
 
 import lombok.extern.java.Log;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationEntryPoint;
 
 @Log
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
     @Bean

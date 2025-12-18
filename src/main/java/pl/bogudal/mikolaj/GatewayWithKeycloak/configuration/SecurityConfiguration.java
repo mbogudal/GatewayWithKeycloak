da60c42d9c3d018d30ba5f79b238c61172f4a9ae
Mikolaj Bogudal <mbogudal@gmail.com>
2025-12-18 18:05:01 +0100

    building project

--- src/main/java/pl/bogudal/mikolaj/GatewayWithKeycloak/configuration/SecurityConfiguration.java
+++ src/main/java/pl/bogudal/mikolaj/GatewayWithKeycloak/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package pl.bogudal.mikolaj.GatewayWithKeycloak.configuration;
 import lombok.extern.java.Log;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.web.server.authentication.RedirectServerAuth
 
 @Log
 @Configuration
+@Profile({"preprod", "prod"})
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
     @Bean
--- src/test/java/pl/bogudal/mikolaj/GatewayWithKeycloak/GatewayWithKeycloakApplicationTests.java
+++ src/test/java/pl/bogudal/mikolaj/GatewayWithKeycloak/GatewayWithKeycloakApplicationTests.java
@@ -2,7 +2,9 @@ package pl.bogudal.mikolaj.GatewayWithKeycloak;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class GatewayWithKeycloakApplicationTests {
 

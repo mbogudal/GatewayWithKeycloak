887f230ca739d012f8e1e72ff03edb4508d84dc8
Mikolaj Bogudal <mbogudal@gmail.com>
2025-12-30 23:23:04 +0100

    working gateway

--- src/main/java/pl/bogudal/mikolaj/GatewayWithKeycloak/configuration/SecurityConfigurationDev.java
+++ src/main/java/pl/bogudal/mikolaj/GatewayWithKeycloak/configuration/SecurityConfigurationDev.java
@@ -3,30 +3,22 @@ package pl.bogudal.mikolaj.GatewayWithKeycloak.configuration;
 import lombok.extern.java.Log;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.RedirectServerAuthenticationEntryPoint;
 
 @Log
 @Configuration
-@Profile({"preprod", "prod",})
+@Profile({ "dev"})
 @EnableWebFluxSecurity
-public class SecurityConfiguration {
+public class SecurityConfigurationDev {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/public/**").permitAll()
-                        .pathMatchers("/**").permitAll()
-                        .pathMatchers("/_next/**", "/favicon.ico", "/robots.txt").permitAll()
-                        .pathMatchers("/api/**").authenticated()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .authorizeExchange(ex -> ex.anyExchange().permitAll())
                 .build();
     }
 }
